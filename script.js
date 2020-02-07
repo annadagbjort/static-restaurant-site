46ef4e58ca56aa8685fb2833df19cbead85d7ef7
annadagbjort <annadagbj@gmail.com>
2020-02-07 12:35:58 +0100

    img work, not pretty

--- script.js
+++ script.js
@@ -1,2 +1,49 @@
+fetch("https://kea-alt-del.dk/t5/api/productlist")
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        showData(data)
+    })
 
 
+
+function showData(jsonData) {
+
+    jsonData.forEach(showSingleDish);
+    console.log(jsonData);
+
+
+}
+
+
+function showSingleDish(dish) {
+    const template = document.querySelector("template").content;
+    const clone = template.cloneNode(true);
+
+    clone.querySelector("h3").textContent = dish.name;
+    clone.querySelector(".description").textContent = dish.shortdescription;
+    clone.querySelector(".price").textContent = dish.price;
+
+
+
+
+
+    const imageName = dish.image; // this would be dynamic
+    console.log(imageName)
+
+    const base = "https://kea-alt-del.dk/t5/site/imgs/";
+
+    const smallImg = base + "small/" + imageName + "-sm.jpg";
+
+    const mediumImg = base + "medium/" + imageName + "-md.jpg";
+    const largeImg = base + "large/" + imageName + ".jpg";
+
+    clone.querySelector("img").src= smallImg;
+
+const parent = document.querySelector("main");
+    parent.appendChild(clone);
+
+
+
+}
